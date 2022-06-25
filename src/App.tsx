cd0d60346db3aa7f863d3e8bbe94cea576626c96
igor ricci <assis.ricci@gmail.com>
2022-06-25 19:18:12 -0300

    add personal notes to index

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
 function App() {
+  const { data } = useQuery(GET_LESSONS_QUERY);
+
+  console.log(data);
+  
+
   return (
-    <h1 className="title">Hello, World.</h1>
+    <h1 className="text-5xl font-bold text-violet-500">Hello, World.</h1>
   )
 }
 
