c776851a173cbf86af35ff5f2b97e5379152e1f8
igor ricci <assis.ricci@gmail.com>
2022-06-26 18:30:31 -0300

    add reactivity to the platform. url will pass the slug which will fetch the video through the api graphcms

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,9 +1,51 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
+import { gql, useQuery } from "@apollo/client";
 import { CaretRight, FileArrowDown, GithubLogo, LinkedinLogo } from "phosphor-react";
 
 import '@vime/core/themes/default.css';
 
-export function Video() {
+const GET_LESSON_BY_SLUG = gql`
+  query GetLessonBySlug ($slug: String) {
+    lesson(where: {slug: $slug}) {
+      title
+      videoId
+      description
+      teacher {
+        name
+        bio
+        avatarURL
+      }
+    }
+  }
+`
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    }
+  }
+}
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+  const { data } = useQuery(GET_LESSON_BY_SLUG, {
+    variables: {
+      slug: props.lessonSlug,
+    }
+  })
+
+  console.log(data);
+
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
