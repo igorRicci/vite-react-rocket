008efc0d15711a9ad4921e59733d41b7ec87a54b
igor ricci <assis.ricci@gmail.com>
2022-06-26 14:25:28 -0300

    add NEW PACKAGE phosphor-react for icons need npm install

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,3 +1,5 @@
+import { CheckCircle } from 'phosphor-react'
+
 export function Lesson() {
   return (
     <a href="#">
@@ -6,16 +8,19 @@ export function Lesson() {
       </span>
 
       <div className="rounded border border-gray-500 p-4 mt-2">
-        <header>
-          <span>
+        <header className="flex items-center justify-between">
+          <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+            <CheckCircle size={20} />
             Check content
           </span>
-          <span>
+          <span className="text-xs rounded py-[0.125rem] px-2 border border-green-300 text-white font-bold">
             LIVE
           </span>
         </header>
 
-        <strong>Dunder grand opening!</strong>
+        <strong className="text-gray-200 mt-5 block">
+          Dunder Mifflin grand opening!
+        </strong>
       </div>
     </a>
   )
