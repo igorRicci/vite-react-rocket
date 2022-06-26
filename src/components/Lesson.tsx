bc055a02b12bb3c6ed1febd1488b2f3902590097
igor ricci <assis.ricci@gmail.com>
2022-06-26 14:10:44 -0300

    add lessons to sidebar

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,22 @@
 export function Lesson() {
   return (
-    <h1>Lesson</h1>
+    <a href="#">
+      <span className="text-gray-300">
+        Tuesday, June 22nd, 19h00
+      </span>
+
+      <div className="rounded border border-gray-500 p-4 mt-2">
+        <header>
+          <span>
+            Check content
+          </span>
+          <span>
+            LIVE
+          </span>
+        </header>
+
+        <strong>Dunder grand opening!</strong>
+      </div>
+    </a>
   )
 }
