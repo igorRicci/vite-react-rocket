85271bf603ecdb5158404a232af3cffe6f579f94
igor ricci <assis.ricci@gmail.com>
2022-06-26 17:21:25 -0300

    add video player

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,10 +1,18 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, FileArrowDown, GithubLogo, LinkedinLogo } from "phosphor-react";
+
+import '@vime/core/themes/default.css';
 
 export function Video() {
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId="pkb2B6OCIY4" />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
