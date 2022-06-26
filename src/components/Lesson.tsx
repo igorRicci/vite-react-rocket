c8af628c3889d0828e921c509f21b0f2e12299fe
igor ricci <assis.ricci@gmail.com>
2022-06-26 14:37:23 -0300

    add propriedades concept to Lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,25 +1,42 @@
-import { CheckCircle } from 'phosphor-react'
+import { CheckCircle, Lock } from 'phosphor-react'
 
-export function Lesson() {
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps) {
+  const isLessonAvailable = false;
+
   return (
     <a href="#">
       <span className="text-gray-300">
-        Tuesday, June 22nd, 19h00
+        {props.availableAt.toString()}
       </span>
 
       <div className="rounded border border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
-          <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+          {isLessonAvailable ? (
+            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
             <CheckCircle size={20} />
             Check content
           </span>
+          ) : (
+            <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+            <Lock size={20} />
+            Available soon!
+          </span>
+          )}
+
           <span className="text-xs rounded py-[0.125rem] px-2 border border-green-300 text-white font-bold">
-            LIVE
+            {props.type === 'live' ? 'LIVE' : 'VIDEO CLASS'}
           </span>
         </header>
 
         <strong className="text-gray-200 mt-5 block">
-          Dunder Mifflin grand opening!
+          {props.title}
         </strong>
       </div>
     </a>
