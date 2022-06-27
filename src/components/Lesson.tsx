f24bd33a1a2b74c86d76baf73118382ffb76349a
igor ricci <assis.ricci@gmail.com>
2022-06-27 17:09:23 -0300

    add conditional css to lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -23,7 +23,9 @@ export function Lesson(props: LessonProps) {
         {availableDateFormatted}
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+      <div
+        className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${isActiveLesson ? 'bg-green-500' : '' }`}
+      >
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
