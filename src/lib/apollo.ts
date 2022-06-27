ceb0f2f80364c3246c53e42c2d6044aafb86ef82
igor ricci <assis.ricci@gmail.com>
2022-06-27 15:08:43 -0300

    add env-local, add to gitignore and api token access

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ub361s0s1o01t660om8nm7/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
