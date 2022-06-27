9b103a2ff432a4f64cfba3546e58de4a257d02d5
igor ricci <assis.ricci@gmail.com>
2022-06-27 17:17:00 -0300

    add LIBRARY classnames. NPM INSTALL. change css condition

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames'
 
 interface LessonProps {
   title: string;
@@ -24,7 +25,9 @@ export function Lesson(props: LessonProps) {
       </span>
 
       <div
-        className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${isActiveLesson ? 'bg-green-500' : '' }`}
+        className={classNames('rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500', {
+          'bg-green-500': isActiveLesson,
+        })}
       >
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
